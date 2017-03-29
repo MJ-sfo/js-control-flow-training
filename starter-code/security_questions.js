7962f5678cc7aaa4e11548e81fee943791c3a678
MJ-sfo <michaeljlaird@yahoo.com>
2017-03-29 13:30:24 -0700

    added window.prompt

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -2,5 +2,5 @@ console.log("security_questions.js is loaded");
 var securityQuestions = [];
 securityQuestions = [{Anne: "dogs"}, {Tim: "coffee"}, {Jen: "daughters"}];
 for (var i = 0; i < securityQuestions.length; i++) {
-	var person = prompt("Please name what is the favorite for: ", securityQuestions[i]);
+	var person = prompt('Please name what is ${securityQuestions[i]} favorite ?';
 }
