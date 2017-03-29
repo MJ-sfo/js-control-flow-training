f90307df8b68495a73431baf2936cc92f6d3ea2b
MJ-sfo <michaeljlaird@yahoo.com>
2017-03-29 15:00:05 -0700

    one more try prompting object

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -2,8 +2,7 @@ console.log("security_questions.js is loaded");
 var securityQuestions = [];
 securityQuestions = [{'Anne': "dogs"}, {'Tim': "coffee"}, {'Jen': "daughters"}];
 for (var i = 0; i < securityQuestions.length; i++) {
-  var name = Object.keys(securityQuestions)[i];
-  // console.log(name);
+  var name = Object.keys(securityQuestions)[i][0];
   console.log(`Please name what is ${name} favorite ?`);
   var concate = (`Please name what is ${name} favorite ?`);
 	var person = prompt(concate);
