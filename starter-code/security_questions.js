b43dd674be8eda5fe374922e89f3f5ba3acb8b27
MJ-sfo <michaeljlaird@yahoo.com>
2017-03-29 13:22:21 -0700

    array of paired objects

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,6 @@
 console.log("security_questions.js is loaded");
+var securityQuestions = [];
+securityQuestions = [{Anne: "dogs"}, {Tim: "coffee"}, {Jen: "daughters"}];
+for (var i = 0; i < securityQuestions.length; i++) {
+	var person = prompt("Please name what is the favorite for: ", securityQuestions[i]);
+}
